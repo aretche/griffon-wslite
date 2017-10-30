88bdf55bc87c15e68425995f69193adccc3c58fd
Gabriel Arellano <arellanog@cent.gov.ar>
2017-10-30 09:55:18 -0300

    Creo una vista básica con JavaFX

--- griffon-app/views/io/github/aretche/griffonWslite/GriffonWsliteView.groovy
+++ griffon-app/views/io/github/aretche/griffonWslite/GriffonWsliteView.groovy
@@ -3,6 +3,14 @@ package io.github.aretche.griffonWslite
 import griffon.core.artifact.GriffonView
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
+import javafx.scene.Scene
+import javafx.scene.control.Tab
+import javafx.scene.control.TabPane
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+import org.kordamp.ikonli.fontawesome.FontAwesome
+import org.kordamp.ikonli.javafx.FontIcon
+
 import javax.annotation.Nonnull
 
 @ArtifactProviderFor(GriffonView)
@@ -12,17 +20,45 @@ class GriffonWsliteView {
     @MVCMember @Nonnull
     GriffonWsliteModel model
 
+    private TabPane tabPane
+
+    @Override
     void initUI() {
-        builder.application(title: application.configuration['application.title'],
-            sizeToScene: true, centerOnScreen: true, name: 'mainWindow') {
-            scene(fill: WHITE, width: 200, height: 60) {
-                gridPane {
-                    label(id: 'clickLabel', row: 0, column: 0,
-                          text: bind(model.clickCountProperty()))
-                    button(row: 1, column: 0, prefWidth: 200,
-                           id: 'clickActionTarget', clickAction)
-                }
+        // Stage sería la ventana
+        Stage stage = (Stage) application.createApplicationContainer([:])
+        // Definimos el título de la ventana
+        stage.title = application.configuration.getAsString('application.title')
+        // Definimos el tamaño de la ventana
+        stage.width = 640
+        stage.height = 480
+        // Deshabilitamos el botón de maximizar
+        stage.setResizable(false)
+
+        stage.scene = init()
+
+        // Creo un tab usando GroovyFX
+        Tab tab1 = new Tab(text: application.messageSource.getMessage('wsLite.tabGroovyFX.label'))
+        builder.with {
+            content = anchorPane {
+                button(leftAnchor: 420, topAnchor: 5, prefWidth: 200,
+                        text: application.messageSource.getMessage('wsLite.refeshButton.label'))
+                textArea(leftAnchor: 20, topAnchor: 40, prefHeight: 360, prefWidth: 600)
             }
         }
+        tab1.graphic = new FontIcon(FontAwesome.GEARS)
+        tab1.content = builder.content
+        tab1.closable = false
+        tabPane.tabs.add(tab1)
+
+        application.windowManager.attach('mainWindow', stage)
     }
+
+    // Construye la Interfaz
+    private Scene init() {
+        tabPane = new TabPane()
+        Scene scene = new Scene(tabPane)
+        scene.fill = Color.WHITE
+        scene
+    }
+
 }
